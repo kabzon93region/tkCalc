c06bf71ab0dd7b144b6cf8aa3cfb26058c1ff2a3
kabzon93region <65691103+kabzon93region@users.noreply.github.com>
2023-04-23 03:42:46 +0300

    0.2
    no math
    yes gui
    yes word functions + replace and dont add "%", "."

--- functions.py
+++ functions.py
@@ -1,65 +1,137 @@
 import tkinter as tk
-def btn_result_function(txt: tk.Entry):
-    print(txt.get())
-    txt.delete(0, 'end')
+
+sym_list = ('+', '-', '*', '/')
+
 
 def btn1_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '1')
 
+
 def btn2_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '2')
+
 
 def btn3_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '3')
 
+
 def btn4_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '4')
+
 
 def btn5_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '5')
 
+
 def btn6_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '6')
+
 
 def btn7_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '7')
 
+
 def btn8_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '8')
+
 
 def btn9_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '9')
 
+
 def btn0_function(txt: tk.Entry):
     print(txt.get())
     txt.insert('end', '0')
 
+
 def btn_plus_function(txt: tk.Entry):
     print(txt.get())
-    txt.insert('end', '+')
+    try:
+        str_str: str = txt.get()
+        if sym_list.index(str_str[len(str_str) - 1]) >= 0:
+            print(f"replace '{str_str[len(str_str) - 1]}' -> '+'")
+            txt.delete(len(str_str) - 1, 'end')
+    except ValueError:
+        print("add '+'")
+    finally:
+        txt.insert('end', '+')
+
 
 def btn_minus_function(txt: tk.Entry):
     print(txt.get())
-    txt.insert('end', '-')
+    try:
+        str_str: str = txt.get()
+        if sym_list.index(str_str[len(str_str) - 1]) >= 0:
+            print(f"replace '{str_str[len(str_str) - 1]}' -> '-'")
+            txt.delete(len(str_str) - 1, 'end')
+    except ValueError:
+        print("add '-'")
+    finally:
+        txt.insert('end', '-')
+
 
 def btn_multi_function(txt: tk.Entry):
     print(txt.get())
-    txt.insert('end', '*')
+    try:
+        str_str: str = txt.get()
+        if sym_list.index(str_str[len(str_str) - 1]) >= 0:
+            print(f"replace '{str_str[len(str_str) - 1]}' -> '*'")
+            txt.delete(len(str_str) - 1, 'end')
+    except ValueError:
+        print("add '*'")
+    finally:
+        txt.insert('end', '*')
+
 
 def btn_split_function(txt: tk.Entry):
     print(txt.get())
-    txt.insert('end', '/')
+    try:
+        str_str: str = txt.get()
+        if sym_list.index(str_str[len(str_str) - 1]) >= 0:
+            print(f"replace '{str_str[len(str_str) - 1]}' -> '/'")
+            txt.delete(len(str_str) - 1, 'end')
+    except ValueError:
+        print("add '/'")
+    finally:
+        txt.insert('end', '/')
+
 
 def btn_percent_function(txt: tk.Entry):
     print(txt.get())
-    txt.insert('end', '%')
+    try:
+        str_str: str = txt.get()
+        if sym_list.index(str_str[len(str_str) - 1]) >= 0:
+            print("not number to percentage")
+    except ValueError:
+        txt.insert('end', '%')
+        print("add '%'")
 
+
+def btn_dot_function(txt: tk.Entry):
+    print(txt.get())
+    try:
+        str_str: str = txt.get()
+        if sym_list.index(str_str[len(str_str) - 1]) >= 0:
+            print("not number to percentage")
+    except ValueError:
+        txt.insert('end', '.')
+        print("add '.'")
+
+
+def btn_result_function(txt: tk.Entry):
+    print(txt.get())
+    # txt.delete(0, 'end')
+    # str_str: str = txt.get()
+    for item in txt.get():
+        print(len(txt.get()))
+    txt.delete(len(txt.get()) - 1, 'end')
+    print()
